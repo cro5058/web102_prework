97cb91362a4e658e23b07f518c8e86b30c415215
Ryan O'Malley <cro5058@g.rit.edu>
2026-05-16 15:51:02 -0400

    Challenge 4 complete.

--- index.js
+++ index.js
@@ -76,19 +76,30 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce( 
+    (acc, game) => {return acc + game.backers}, 
+    0
+);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = totalContributions.toLocaleString('en-US');
 
-
-// grab the amount raised card, then use reduce() to find the total amount raised
+// grab the amount raised card
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+// use reduce() to find the total amount raised
+const totalRaised = GAMES_JSON.reduce(
+    (acc, game) => {return acc + game.pledged},
+    0
+)
 
+// set inner HTML using template literal
+raisedCard.innerHTML = totalRaised.toLocaleString('en-US');
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+gamesCard.innerHTML = GAMES_JSON.length;
 
 
 /*************************************************************************************
